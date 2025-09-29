4dd7b12906b50209904742f4e58c8a94c84e4122
everettjf <everettjf@live.com>
2025-09-28 18:05:58 -0700

    Initial Commit

---
+++ Spello/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spello
+//
+//  Created by eevv on 9/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spello/SpelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpelloApp.swift
+//  Spello
+//
+//  Created by eevv on 9/28/25.
+//
+
+import SwiftUI
+
+@main
+struct SpelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
